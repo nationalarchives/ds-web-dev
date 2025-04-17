f15d95933958c4266ef9140194a0ef5e68fd5feb
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-04-17 17:27:45 +0100

    Add initial project setup with Docker, Nginx configuration, and development scripts

---
+++ README.md
@@ -0,0 +1,11 @@
+# TNA Website Local Development
+
+A local development setup for all the services required to run `nationalarchives.gov.uk`.
+
+## Quickstart
+
+```sh
+./init.sh
+```
+
+The required services will be cloned into the `services` directory. From there, you may need to add the required `.env` files to get them to work.
---
+++ init.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -e
+
+docker compose down
+
+# Generate a self-signed SSL certificate for localhost
+mkdir -p ssl
+if [[ ! -f ssl/localhost.crt || ! -f ssl/localhost.key ]];
+then
+    echo "Generating self-signed SSL certificate for localhost..."
+    openssl req -x509 -out ssl/localhost.crt -keyout ssl/localhost.key \
+        -newkey rsa:2048 -nodes -sha256 \
+        -subj '/CN=localhost' -extensions EXT -config <( \
+        printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+fi
+
+# Make a directory for the services
+mkdir -p services
+
+# Define the list of services
+declare -a services=(
+    "ds-frontend" 
+    "ds-wagtail" 
+    "ds-search"
+    "ds-frontend-enrichment"
+    "ds-sitemap-search"
+)
+
+# Clone the services if they don't exist and start them
+for service in "${services[@]}"
+do
+    if [ ! -d "services/$service" ]
+    then
+        echo "Cloning $service..."
+        git clone "git@github.com:nationalarchives/$service.git" "services/$service"
+    fi
+    echo "Starting $service..."
+    docker compose --file "services/$service/docker-compose.yml" up --detach && echo "✅ Started $service" || echo "❌ Failed to start $service"
+    echo
+done
+
+# Set up the frontend dependencies for development
+echo "Set up the frontend dependencies..."
+docker compose --file services/ds-frontend/docker-compose.yml exec app cp -r /app/node_modules/@nationalarchives/frontend/nationalarchives/assets /app/app/static
+docker compose --file services/ds-frontend-enrichment/docker-compose.yml exec app cp -r /app/node_modules/@nationalarchives/frontend/nationalarchives/assets /app/app/static
+docker compose --file services/ds-search/docker-compose.yml exec app cp -r /app/node_modules/@nationalarchives/frontend/nationalarchives/assets /app/app/static
+docker compose --file services/ds-sitemap-search/docker-compose.yml exec app cp -r /app/node_modules/@nationalarchives/frontend/nationalarchives/assets /app/app/static
+
+# Start the nginx service
+echo "Starting nginx..."
+docker compose up --build --detach && echo "✅ Started nginx" || echo "❌ Failed to start nginx"
+
+# Pull a copy of the development database
+echo "Pulling a copy of the development database..."
+docker compose --file services/ds-wagtail/docker-compose.yml exec dev pull
+
+# Populate the sitemap search database in the background
+echo "Populating the sitemap search database..."
+docker compose --file services/ds-sitemap-search/docker-compose.yml exec --detach app poetry run python populate.py
+
+# Open the browser
+xdg-open https://localhost || open https://localhost || start https://localhost
