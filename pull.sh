86587d646d906dc7c04b69c1587e63fb0d3fa391
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-05-30 15:51:04 +0100

    Remove redundant Docker compose build on pull.sh

--- pull.sh
+++ pull.sh
@@ -16,7 +16,7 @@ do
     cd "services/$service"
     BRANCH="$(git rev-parse --abbrev-ref HEAD)"
     if [[ "$BRANCH" == "main" ]]; then
-        git pull origin main && docker compose build && echo "✅ Pulled latest changes for $service" || echo "❌ Failed to pull latest changes for $service"
+        git pull origin main && echo "✅ Pulled latest changes for $service" || echo "❌ Failed to pull latest changes for $service"
     else
         echo "⚠️ You are on the $BRANCH branch. Please switch to the main branch to pull the latest changes."
     fi
