ded2cbacb0b95366308f69dafc39d7ec94562c43
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-01-06 11:04:19 +0000

    Enhance prune script to clear git stashes after pruning old branches

--- prune.sh
+++ prune.sh
@@ -8,13 +8,14 @@ source services.sh
 # Prune all old branches for each service
 for service in "${services[@]}"
 do
-    echo "Pruning old branches for $service..."
+    echo "Pruning old branches and clearing git stashes for $service..."
     cd "services/$service"
     git fetch -p
     for branch in $(git branch -vv | grep ': gone]' | awk '{print $1}')
     do
         git branch -D $branch
     done
+    git stash clear
     cd "../.."
     echo
 done
