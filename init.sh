ecfdfd1199288a0831b18efb6c68ca2382d6a459
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-02-11 16:16:55 +0000

    Update init.sh to create Wagtail API token and refresh frontend .env files

--- init.sh
+++ init.sh
@@ -100,7 +100,7 @@ fi
 
 # Create an API token for the Wagtail admin user and update ds-frontend and wa-frontend .env files
 echo "Creating Wagtail API token and updating frontend .env files..."
-API_TOKEN=$(docker compose --file services/ds-wagtail/docker-compose.yml exec app poetry run python manage.py drf_create_token admin | sed -r -e 's/Generated token ([0-9a-f]+) for user admin/\1/')
+API_TOKEN=$(docker compose --file services/ds-wagtail/docker-compose.yml exec app poetry run python manage.py manage_api_token localhost --refresh --quiet)
 sed -i .bak -r -e 's/WAGTAIL_API_KEY=[^\n]*/WAGTAIL_API_KEY='"$API_TOKEN"'/' services/ds-frontend/.env
 rm -f services/ds-frontend/.env.bak
 docker compose --file "services/ds-frontend/docker-compose.yml" up --detach --wait --wait-timeout 120 app
@@ -108,6 +108,8 @@ sed -i .bak -r -e 's/WAGTAIL_API_KEY=[^\n]*/WAGTAIL_API_KEY='"$API_TOKEN"'/' ser
 rm -f services/wa-frontend/.env.bak
 docker compose --file "services/wa-frontend/docker-compose.yml" up --detach --wait --wait-timeout 120 app
 rm -f .env
+
+# Update the root .env file with the Wagtail API token for use by the documentation site
 echo "WAGTAIL_API_KEY=$API_TOKEN" >> .env
 docker compose up --detach --wait --wait-timeout 120 wagtail-docs
 
