41dbe2d565418d7efbc7da6e1ed52cb0d99b228d
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-06-17 13:59:03 +0100

    Add --no-min-release-age option for npm dependency upgrades and update parameter descriptions

--- upgrade_npm_dependency.sh
+++ upgrade_npm_dependency.sh
@@ -13,10 +13,18 @@ then
   echo -e "Error: application directory not passed\n";
   echo "PARAMETERS"
   echo "  application          the directory of the application to upgrade"
-  echo "  tna-frontend         the version of TNA Frontend to use"
-  echo "  [tna-frontend-jinja] the version of TNA Frontend Jinja to use (optional)"
+  echo "  dependency           the name of the dependency to upgrade"
+  echo "  version              the version of the dependency to use"
   echo "                       Example: upgrade_npm_dependency.sh services/ds-frontend @nationalarchives/frontend 0.30.2";
   exit 1
+fi
+
+# If --no-min-release-age is passed, allow packages released less than the cooldown period
+if [[ "$4" == "--no-min-release-age" ]]
+then
+  MIN_RELEASE_AGE_FLAG="--min-release-age=0"
+else
+  MIN_RELEASE_AGE_FLAG=""
 fi
 
 # Check if the application directory contains a docker-compose.yml file
@@ -32,7 +40,7 @@ then
     if jq "has(.dependencies.\"$DEPENDENCY_NAME\")" "$APPLICATION_DIRECTORY/package.json" > /dev/null;
     then
         echo "Updating $DEPENDENCY_NAME to version $DEPENDENCY_VERSION"
-        docker compose --file "$APPLICATION_DIRECTORY/docker-compose.yml" exec app bash -c ". tna-nvm && npm install --save-exact $DEPENDENCY_NAME@$DEPENDENCY_VERSION"
+        docker compose --file "$APPLICATION_DIRECTORY/docker-compose.yml" exec app bash -c ". tna-nvm && npm install --save-exact $DEPENDENCY_NAME@$DEPENDENCY_VERSION $MIN_RELEASE_AGE_FLAG"
     else
         echo "$DEPENDENCY_NAME not found in package.json, skipping..."
     fi
